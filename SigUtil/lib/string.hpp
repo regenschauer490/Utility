530af0c2322a3bd7f77eaf29a3625b05083b6574
Akihiro Nishimura <hagure.liminality@hotmail.co.jp>
2014-02-11 06:07:26 +0900

    stable: string.hpp

--- SigUtil/lib/string.hpp
+++ SigUtil/lib/string.hpp
@@ -1,3 +1,26 @@
+/*
+The MIT License(MIT)
+
+Copyright(c) 2014 Akihiro Nishimura
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files(the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #ifndef __SIG_UTIL_STRING__
 #define __SIG_UTIL_STRING__
 
@@ -150,21 +173,6 @@ namespace sig{
 		return Split<Container>(std::wstring(src), std::wstring(delimiter));
 	}
 
-	/*
-#if SIG_ENABLE_BOOST
-	//コンテナに格納された全文字列を結合して1つの文字列に(delimiterで区切り指定)
-	template < class T, template < class T, class = std::allocator<T >> class Container >
-	inline std::string CatStr(Container<T> const& container, std::string delimiter = "")
-	{
-	std::string tmp;
-
-	for (auto const& src : container){
-	tmp += (boost::format("%1%%2%") % src % delimiter).str();
-	}
-	return std::move(tmp);
-	}
-#endif
-	*/
 
 	//コンテナに格納された全文字列を結合して1つの文字列に(delimiterで区切り指定)
 	template <class C, class T>
