4d894ef1fe3c57c228fc49f7a44ba678baf4cbf1
regenschauer490 <hagure.liminality@hotmail.co.jp>
2014-02-12 22:13:04 +0900

    2_12

--- SigUtil/lib/set.hpp
+++ SigUtil/lib/set.hpp
@@ -1,26 +1,3 @@
-/*
-The MIT License(MIT)
-
-Copyright(c) 2014 Akihiro Nishimura
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files(the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions :
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 #ifndef __SIG_UTIL_SET__
 #define __SIG_UTIL_SET__
 
@@ -51,14 +28,16 @@ namespace sig
 #endif
 
 	//ソート前のindexを保持してソート
-	template <class T>
-	std::vector< std::tuple<uint, T> > SortWithIndex(std::vector<T> const& vec, bool const small_to_large)
+	template <class C>
+	auto SortWithIndex(C const& vec, bool small_to_large)
 	{
+		useing T = typename container_traits<C>::value_type;
 		std::vector< std::tuple<uint, T> > result(vec.size());
 
-		for (uint i = 0; i < vec.size(); ++i){
+		auto it = std::begin(vec);
+		for (uint i = 0; i < vec.size(); ++i, ++it){
 			std::get<0>(result[i]) = i;
-			std::get<1>(result[i]) = vec[i];
+			std::get<1>(result[i]) = *it;
 		}
 		if (small_to_large) std::sort(result.begin(), result.end(), [](std::tuple<uint, T> const& a, std::tuple<uint, T> const& b){ return std::get<1>(a) < std::get<1>(b); });
 		else std::sort(result.begin(), result.end(), [](std::tuple<uint, T> const& a, std::tuple<uint, T> const& b){ return std::get<1>(b) < std::get<1>(a); });
