4617b5e6988cb3e80a7b4feda79978222f085a65
Akihiro Nishimura <hagure.liminality@hotmail.co.jp>
2014-10-29 09:07:07 +0900

    add compile setting preset in project

--- project/clang 3.4/clang.mk
+++ project/clang 3.4/clang.mk
@@ -4,10 +4,10 @@ LDFLAGS  =
 STDLIB	= -I/usr/include/c++/4.8.1 -I/usr/include/i386-linux-gnu/c++/4.8
 INCLUDE_B  = -I/usr/include/c++/boost/boost_1_55_0
 LIBS     = /usr/include/c++/boost/boost_1_55_0/stage/lib/libboost_system.a /usr/include/c++/boost/boost_1_55_0/stage/lib/libboost_filesystem.a /usr/include/c++/boost/boost_1_55_0/stage/lib/libboost_regex.a 
-BIN_DIR  = ../bin
+BIN_DIR  = bin
 TARGET	= test
 TARGET0   = $(addprefix $(BIN_DIR)/,$(TARGET))
-OBJ_DIR   = obj/clang
+OBJ_DIR   = obj
 
 INCLUDE = $(STDLIB) $(INCLUDE_B)
-include impl.mk
+include ../impl.mk
--- project/gcc 4.8.1/gcc.mk
+++ project/gcc 4.8.1/gcc.mk
@@ -3,9 +3,9 @@ CFLAGS   = -Wextra -g -MMD -MP -std=gnu++1y
 LDFLAGS  =
 INCLUDE  = -I/usr/include/c++/boost/boost_1_55_0
 LIBS     = /usr/include/c++/boost/boost_1_55_0/stage/lib/libboost_system.a /usr/include/c++/boost/boost_1_55_0/stage/lib/libboost_filesystem.a /usr/include/c++/boost/boost_1_55_0/stage/lib/libboost_regex.a 
-BIN_DIR  = ../bin
+BIN_DIR  = bin
 TARGET	= test
 TARGET0   = $(addprefix $(BIN_DIR)/,$(TARGET))
-OBJ_DIR   = obj/gcc
+OBJ_DIR   = obj
 
-include impl.mk
+include ../impl.mk
